adf5b197d70ac767c532f7be9e2c2363de49b6e4
antonioLucianoNeves <a_luciano10@hotmail.com>
2022-02-20 12:10:17 -0300

    Area Triangulo calculo

---
+++ AreaRetangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AreaRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            
+
+            Console.WriteLine("Entre com a largura de altura do retangulo!");
+            ret.altura = int.Parse(Console.ReadLine());
+
+            ret.largura = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Area: " + ret.Area());
+
+            Console.WriteLine("Perimetro: ");
+
+            Console.WriteLine("Diagonal: ");
+
+        }
+    }
+}
---
+++ AreaRetangulo/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaRetangulo
+{
+    class Retangulo
+    {
+        public int altura;
+        public int largura;
+
+        public int Area() {
+            return altura * largura;
+
+            
+        }
+        public int Perimetro()
+        {
+            return altura * largura;
+
+        }
+        public int Diagonal()
+        {
+            return altura * largura;
+        }
+    }
+    
+    
+
+}
